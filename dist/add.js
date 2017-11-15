f7320d50d3d69e3c2caf584365c44cc8f11c4766
hongmaoxiao <769501294@qq.com>
2017-11-16 07:09:28 +0800

    modern syntax

--- dist/add.js
+++ dist/add.js
@@ -1,2 +1,2 @@
-!function(){"use strict";var t=document.querySelector("#add"),e=function(t,e){return t+e}(1,2);t.textContent=e}();
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t():"function"==typeof define&&define.amd?define(t):t()}(0,function(){"use strict";var e=document.querySelector("#add"),t=function(e,t){return e+t}(1,2);e.textContent=t});
 //# sourceMappingURL=add.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,13 @@ import uglify from 'rollup-plugin-uglify';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  entry: 'src/main.js',
-  dest: 'dist/add.js',
-  format: 'iife',
+  input: 'src/main.js',
+  output: {
+    file: 'dist/add.js',
+    format: 'umd'
+  },
   plugins: [
     production && uglify()
   ],
-  sourceMap: true
+  sourcemap: true
 }
